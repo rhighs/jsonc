3cd30c3e5321718edab8fc7c2b6dd0c5182f7e38
roberto montalti <roberto.montalti00@gmail.com>
2023-07-13 08:51:47 +0200

    Example of nested object's property access

--- main.c
+++ main.c
@@ -19,25 +19,25 @@ i32 main(void) {
  
     if (JSON_EXISTS(value, "ciao")
             && JSON_TYPE(value, "ciao") == JSON_TYPE_NUMBER) {
-        double number = JSON_GET(value, "ciao", double);
+        double number = JSON_GET(value, double, "ciao");
         printf("Found number: %f\n", number);
     }
 
     if (JSON_EXISTS(value, "name")
             && JSON_TYPE(value, "name") == JSON_TYPE_STRING) {
-        const char *name = JSON_GET(value, "name", const char *);
+        const char *name = JSON_GET(value, const char *, "name");
         printf("Found name: %s\n", name);
     }
 
     if (JSON_EXISTS(value, "truthy")
             && JSON_TYPE(value, "truthy") == JSON_TYPE_BOOL) {
-        u8 b = JSON_GET(value, "truthy", u8);
+        u8 b = JSON_GET(value, u8, "truthy");
         printf("Found truthy: %d\n", b);
     }
 
     if (JSON_EXISTS(value, "falsey")
             && JSON_TYPE(value, "falsey") == JSON_TYPE_BOOL) {
-        u8 b = JSON_GET(value, "falsey", u8);
+        u8 b = JSON_GET(value, u8, "falsey");
         printf("Found falsey: %d\n", b);
     }
 
@@ -49,20 +49,28 @@ i32 main(void) {
     JSON_SET(value, "ciaociao", JSON_TYPE_NUMBER, double, 100.0);
     if (JSON_EXISTS(value, "ciaociao")
             && JSON_TYPE(value, "ciaociao") == JSON_TYPE_NUMBER) {
-        double number = JSON_GET(value, "ciaociao", double);
+        double number = JSON_GET(value, double, "ciaociao");
         printf("Found number: %f\n", number);
     }
 
     json_value_t o = JSON_OBJECT(
             JSON_PROP("test", JSON_NUMBER(9999.0)),
             JSON_PROP("value", JSON_OBJECT(
-                    JSON_PROP("test", JSON_NUMBER(100.0))
+                    JSON_PROP("test", JSON_NUMBER(100.0)),
+                    JSON_PROP("test_object", JSON_OBJECT(
+                            JSON_PROP("nested", JSON_STRING("Some deeply nested string"))
+                            ))
                     ))
     );
 
+    const char *nested_str =
+        JSON_GET(o, const char*, "value", "test_object", "nested");
+
+    printf("Value str: %s\n", nested_str);
+
     if (JSON_EXISTS(o, "test")
             && JSON_TYPE(o, "test") == JSON_TYPE_NUMBER) {
-        double number = JSON_GET(o, "test", double);
+        double number = JSON_GET(o, double, "test");
         printf("Found number: %f\n", number);
     }
  
